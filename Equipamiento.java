c195ba16143f3ef97b923a058d826adcb85803ff
aleixmv10 <aleixmv10@alumnes.ilerna.com>
2026-02-11 18:02:43 +0100

    public equipamiento
    dos private de string nombre y tipo con un public dnde esta el nombre del equipamiento

---
+++ Equipamiento.java
@@ -0,0 +1,14 @@
+package javones_mazmorras;
+
+public class Equipamiento {
+
+    private String nombre;
+    private String tipo;
+
+    public Equipamiento (String nombre, String tipo) {
+         this.nombre = nombre;
+        this.tipo = tipo;
+    
+    }
+
+}
