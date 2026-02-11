7768ed06869e86ba019447265f0b3aee93f2bde6
aleixmv10 <aleixmv10@alumnes.ilerna.com>
2026-02-11 18:56:56 +0100

    Public Ladron
    String de nobre 2 ints nivel int vida

---
+++ Ladron.java
@@ -0,0 +1,19 @@
+package javones_mazmorras;
+
+public class Ladron extands personajes{
+
+    private boolean invisible;
+
+    public Ladron(String nombre, int nivel, int vida) {
+        super(nombre, nivel, vida);
+        invisible = false;
+    }
+    
+
+
+
+
+
+
+
+}
