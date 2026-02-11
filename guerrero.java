0d0b2c541a0340d7348066561d00246cbbb2d8f5
aleixmv10 <aleixmv10@alumnes.ilerna.com>
2026-02-11 17:49:35 +0100

    publicguerrero
    un public guerrero con string de nombre int de vida y nivel

---
+++ guerrero.java
@@ -0,0 +1,12 @@
+package javones_mazmorras;
+
+public class guerrero extends personajes {
+     private Personaje protegido;
+    
+     public guerrero (String nombre, int nivel, int vida){
+        super(nombre, nivel, vida);
+     }
+
+
+
+}
