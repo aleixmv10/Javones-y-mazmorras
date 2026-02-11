989361be5def6ec217d8cab1d1699d1351a7ef4d
aleixmv10 <aleixmv10@alumnes.ilerna.com>
2026-02-11 19:17:19 +0100

    lanzar hechizo
    un public con un if y un else

---
+++ mago.java
@@ -0,0 +1,20 @@
+package javones_mazmorras;
+
+public class mago extends personajes {
+	private int mana;
+	
+	public mago ( String nombre, int nivel, int vida, int mana) {
+	super();
+	
+		this.mana = mana;
+	}
+	 public void lanzarHechizo(int costeMana) {
+        if (mana >= costeMana) {
+            mana = mana - costeMana;
+            System.out.println(getNombre() + " lanza un hechizo.");
+        } else {
+            System.out.println("No hay suficiente maná.");
+        }
+    }
+	
+}
