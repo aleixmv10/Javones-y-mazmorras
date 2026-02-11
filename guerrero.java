1b193ea145a76f4bbb9692655e940ba04e82c91a
aleixmv10 <aleixmv10@alumnes.ilerna.com>
2026-02-11 17:55:28 +0100

    Public void de proteger y dejar de protejer
    2 publics mas

--- guerrero.java
+++ guerrero.java
@@ -6,7 +6,18 @@ public class guerrero extends personajes {
      public guerrero (String nombre, int nivel, int vida){
         super(nombre, nivel, vida);
      }
-
-
+      
+     public void proteger(Personaje p) {
+        protegido = p;
+        p.proteger();
+        System.out.println(getNombre() + " protege a " + p.getNombre());
+    }
+   
+    public void dejarDeProteger() {
+        if (protegido != null) {
+            protegido.desproteger();
+            protegido = null;
+        }
+    }
 
 }
